5930eb6060a3d87da2fde424a7427bc1986e934d
rvolkov8 <120594195+rvolkov8@users.noreply.github.com>
2023-01-06 13:59:18 +0200

    Create a function that randomly returns the computer’s choice

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        console.log("Rock");
+    } else if (randomNumber === 1) {
+        console.log("Paper");
+    } else {
+        console.log("Scissors");
+    }
+};
+
+getComputerChoice();
