a504cf069fa12aa8acc9113ca78681049d93957e
rvolkov8 <120594195+rvolkov8@users.noreply.github.com>
2023-01-06 14:44:11 +0200

    Create a function that plays a single round of the game
    – Also make your function’s first parameter case-insensitive
    – Fix the getComputerChoice func to return instead of logging the values

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
-        console.log("Rock");
+        return "Rock";
     } else if (randomNumber === 1) {
-        console.log("Paper");
+        return "Paper";
     } else {
-        console.log("Scissors");
+        return "Scissors";
     }
 };
 
-getComputerChoice();
+function startGame(playerSelection, computerSelection) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You win! Rock beats Scissors"
+    } else if (playerSelection === "Rock" && computerSelection === "Rock") {
+        return "Draw! Let's play again"
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        return "You lose! Scissors beats Paper"
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You win! Paper beats Rock"
+    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+        return "Draw! Let's play again"
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        return "You lose! Rock beats Scissors"
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You win! Scissors beats Paper"
+    } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+        return "Draw! Let's play again"
+    } 
+};  
+
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice();
+console.log(startGame(playerSelection,computerSelection));
+
